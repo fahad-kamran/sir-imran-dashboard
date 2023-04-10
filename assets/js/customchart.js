615ed23398ec91de0467eed5a7d31f88fb039256
fahad-kamran <fahadcs512@gmail.com>
2023-04-10 23:50:35 +0500

    second-day 10-APR-2023

--- assets/js/customchart.js
+++ assets/js/customchart.js
@@ -1,4 +1,5 @@
-var expenses_options = {
+// chart 1 start
+let expenses_options = {
     series: [44, 55, 41, 17, 15],
     labels: ['Salary', 'Electricity', 'Telephone', 'Printing', 'Transport'],
     colors: ['#114a82', '#00a31f', '#ffa900', '#08cafe', '#dc4c41'],
@@ -19,18 +20,19 @@ var expenses_options = {
     }]
 };
 
-var expenses = new ApexCharts(document.querySelector('#expences_chart_donut'), expenses_options);
+let expenses = new ApexCharts(document.querySelector('#expences_chart_donut'), expenses_options);
 expenses.render();
+// chart 1 end
 
-
-var monthly_turnover_options = {
+// chart 2 start
+let monthly_turnover_options = {
     series: [{
         data: [400, 430, 448, 470, 540, 580],
     }],
-    colors: ['#114a82', '#00a31f', '#ffa900', '#08cafe', '#dc4c41'],
+    colors: ['#114a82'],
     chart: {
         type: 'bar',
-        height: 250
+        height: 200
     },
     plotOptions: {
         bar: {
@@ -46,11 +48,12 @@ var monthly_turnover_options = {
     }
 };
 
-var monthly_turnover = new ApexCharts(document.querySelector("#monthly_tournover_chart_bar"), monthly_turnover_options);
+let monthly_turnover = new ApexCharts(document.querySelector("#monthly_tournover_chart_bar"), monthly_turnover_options);
 monthly_turnover.render();
+// chart 2 end
 
-
-var invoices_options = {
+// chart 3 start
+let invoices_options = {
     series: [{
         data: [44, 55, 41, 64, 22, 43, 21]
     }, {
@@ -59,7 +62,7 @@ var invoices_options = {
     colors: ['#00a31f', '#08cafe'],
     chart: {
         type: 'bar',
-        height: 250
+        height: 200
     },
     plotOptions: {
         bar: {
@@ -91,5 +94,60 @@ var invoices_options = {
     },
 };
 
-var total_invoices_paid = new ApexCharts(document.querySelector("#invoice_paid"), invoices_options);
+let total_invoices_paid = new ApexCharts(document.querySelector("#invoice_paid"), invoices_options);
 total_invoices_paid.render();
+// chart 3 end
+
+// chart 4 start
+var topFiveCustomers = {
+    series: [{
+        data: [50, 70, 80, 90, 95]
+    }],
+    chart: {
+        type: 'bar',
+        height: 250
+    },
+    plotOptions: {
+        bar: {
+            borderRadius: 4,
+            horizontal: false,
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    xaxis: {
+        categories: ['customer1', 'customer2', 'customer3', 'customer4', 'customer5'],
+    }
+};
+
+var topFiveChart = new ApexCharts(document.querySelector("#top_five_customers"), topFiveCustomers);
+topFiveChart.render();
+// chart 4 end
+
+// chart 5 start
+var annualRecord = {
+    series: [44, 55, 13, 43, 22],
+    chart: {
+        width: 380,
+        type: 'pie',
+    },
+    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+    colors: ['#114a82', '#00a31f', '#ffa900', '#08cafe', '#dc4c41'],
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: '100%',
+                height: 300,
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var AnnualChart = new ApexCharts(document.querySelector("#annual_record"), annualRecord);
+AnnualChart.render();
+// chart 5 end
